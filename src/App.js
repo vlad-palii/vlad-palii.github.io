d6f3abb75daa21a8224a786db8e79bfe55db57a0
Vlad <paliy.work@gmail.com>
2020-02-16 20:17:20 +0200

    Fixed icons, text, etc.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import './App.scss';
 import Home from './components/homeComponent'
 import AirQuality from './components/airQulityComponent'
+import CodingProblems from './components/codingProblems'
 
 export default function App() {
 
@@ -16,23 +17,31 @@ export default function App() {
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light ">
         <a className="navbar-brand" href="#"></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ">
             <li className="nav-item">
               <NavLink exact className="nav-link" activeClassName="active" to="/">CV</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink exact className="nav-link" activeClassName="active" to="/air-quality-pet">Air Quality</NavLink>
+            <NavLink exact className="nav-link" activeClassName="active" to="/air-quality">Air Quality</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink exact className="nav-link" activeClassName="active" to="/coding-problems">Coding Problems</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink exact className="nav-link" activeClassName="active" to="/anguar-chat">Angular 8 Chat</NavLink>
             </li>
           </ul>
         </div>
       </nav>
 
       <Route exact path="/" component={Home} />
-      <Route exact path="/air-quality-pet" component={AirQuality} />
+      <Route exact path="/air-quality" component={AirQuality} />
+      <Route exact path="/coding-problems" component={CodingProblems} />
+      <Route exact path="/anguar-chat"  />
     </Router>
 
     </div>
---
+++ src/components/codingProblems.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import {Highlight} from 'react-highlight.js';
+
+export default function CodingProblems (){
+  return (
+    <div>
+    
+      <Container>
+        <h2 className="page-header">Air Quality in your location (React hooks):</h2>
+        <Row>
+        
+        </Row>
+      </Container>
+        
+    </div>  
+  )
+};
