f3779acc240c9ffff2298863ad38770619bf7eb4
Vlad Palii <vlad_burner@Vlads-MacBook-Pro.local>
2022-02-14 17:11:12 +0200

    Updates

---
+++ precache-manifest.c6ce039038b22a517b69594f9f15505a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcb8fb19a9d0b47c4ccea736aefeb32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb7bd3a3b41f1c781169",
+    "url": "./static/css/main.11bbbbe9.chunk.css"
+  },
+  {
+    "revision": "483a973cce728be98dbb",
+    "url": "./static/js/2.36a9d7d9.chunk.js"
+  },
+  {
+    "revision": "eb7bd3a3b41f1c781169",
+    "url": "./static/js/main.54ffdba4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "faef965e90e8e522c9f9dc7b6f1d94e5",
+    "url": "./static/media/avatar.faef965e.jpg"
+  },
+  {
+    "revision": "e465ab6949cee40be5c31736126440c2",
+    "url": "./static/media/dead-leaf.e465ab69.jpg"
+  },
+  {
+    "revision": "2f7fccf428118cb306bb5e235af29ccf",
+    "url": "./static/media/health-leaf.2f7fccf4.jpg"
+  },
+  {
+    "revision": "8f8671d13698e824a9cf5b85a5515e4d",
+    "url": "./static/media/icons.8f8671d1.woff2"
+  },
+  {
+    "revision": "c1d7272cd416b35ed29c26a5896c3737",
+    "url": "./static/media/sick-leaf.c1d7272c.jpg"
+  }
+]);
